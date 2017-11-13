b5ecc2c5f6c5a0623c82c1bac23cc7cabe457218
Michele Martone <michelemartone@users.sourceforge.net>
2017-11-13 17:34:15 +0100

    commented the script and filtering a bit more

--- mimoch.sh
+++ mimoch.sh
@@ -2,25 +2,24 @@
 module purge
 MY_MODULEPATH=${1:-$MODULEPATH}
 #MY_MODULEPATH='/lrz/sys/share/modules/extfiles'
-for PD in  ${MY_MODULEPATH//:/ } ; do
-for MF in `find ${PD} -type f `; do
-	bn=`basename ${MF}`; test ${bn:0:1} = . && continue
-	MN=`echo ${MF} | sed 's/\s\s*/ /g' | rev | awk  -F / '{print $1"/"$2 }'| rev` 
-	module avail ${MN} > /dev/null || { echo "internal error!"; exit 1; }
-	for PVID in 'prepend-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>'; do
+for MD in  ${MY_MODULEPATH//:/ } ; do # modules directory
+for MF in `find ${MD} -type f `; do # module file
+	bn=`basename ${MF}`; test ${bn:0:1} = . && continue # no hidden files
+	MN=`echo ${MF} | sed 's/\s\s*/ /g' | rev | awk  -F / '{print $1"/"$2 }'| rev` # module name
+	module avail ${MN} > /dev/null || { echo "internal error!"; exit 1; } # we assert module to be valid
+	for PVID in 'prepend-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>'; do # path variable identifier expressions
 	if test -f ${MF} && grep -l '#%Module' >/dev/null ${MF}  && \
-		#MPL="`module show $MF | sed 's/\s\s*/ /g' | grep -v '^\(--\|module-whatis\|  *\)' | grep '^[\s]\+' `" && \
-		MPL="`module show ${MF} | sed 's/\s\s*/ /g' | grep "^${PVID} .*$"  `" && \
+		MPL="`module show ${MF} | sed 's/\s\s*/ /g' | grep "^${PVID} .*$" | grep -v '^\(--\|module-whatis\|  *\)'  `" && \
 		MC="`echo ${MPL} | awk  -F ' ' '{print $1 }'`" && \
 		MI="`echo ${MPL} | awk  -F ' ' '{print $2 }'`" && \
 		MV="`echo ${MPL} | awk  -F ' ' '{print $3 }'`"  && \
 		MA=`echo "${MC} ${MI}" | grep "${PVID}" ` && \
-		test -n "$MA" ; 
+		test -n "$MA" ; # matching assignment
 		then
-		for P in ${MV//:/ }; do
-			test -d ${P} || echo "module ${MN} [${MF}] : ${MI}=\"$P\" not a directory!" ; 
+		for PD in ${MV//:/ }; do # path directory
+			test -d ${PD} || echo "module ${MN} [${MF}] : ${MI}=\"$PD\" not a directory!" ; 
 		done; 
 	fi  ; 
 	done;
-done    ;  
-done #| grep is.not
+done    ; 
+done	;
