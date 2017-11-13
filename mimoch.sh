eb808a825ad574919bb8751e844e5eb6e1ceebdc
Michele Martone <michelemartone@users.sourceforge.net>
2017-11-13 17:43:54 +0100

    append-path will be captured too

--- mimoch.sh
+++ mimoch.sh
@@ -7,7 +7,8 @@ for MF in `find ${MD} -type f `; do # module file
 	bn=`basename ${MF}`; test ${bn:0:1} = . && continue # no hidden files
 	MN=`echo ${MF} | sed 's/\s\s*/ /g' | rev | awk  -F / '{print $1"/"$2 }'| rev` # module name
 	module avail ${MN} > /dev/null || { echo "internal error!"; exit 1; } # we assert module to be valid
-	for PVID in 'prepend-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>' 'setenv .*BASE\>'; do # path variable identifier expressions
+	for PVID in '\(pre\|ap\)pend-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>' 'setenv .*BASE\>'; do # path variable identifier expressions
+	#for PVID in '.p[p]end-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>' 'setenv .*BASE\>'; do # path variable identifier expressions
 	if test -f ${MF} && grep -l '#%Module' >/dev/null ${MF}  && \
 		MPL="`module show ${MF} | sed 's/\s\s*/ /g' | grep "^${PVID} .*$" | grep -v '^\(--\|module-whatis\|  *\)'  `" && \
 		MC="`echo ${MPL} | awk  -F ' ' '{print $1 }'`" && \
