4cdbf9a94a4fda74115a108a95e1bb799411ca8e
Michele Martone <michelemartone@users.sourceforge.net>
2017-11-13 17:38:22 +0100

    testing for setenv.*BASE like patterns as well.

--- mimoch.sh
+++ mimoch.sh
@@ -7,13 +7,13 @@ for MF in `find ${MD} -type f `; do # module file
 	bn=`basename ${MF}`; test ${bn:0:1} = . && continue # no hidden files
 	MN=`echo ${MF} | sed 's/\s\s*/ /g' | rev | awk  -F / '{print $1"/"$2 }'| rev` # module name
 	module avail ${MN} > /dev/null || { echo "internal error!"; exit 1; } # we assert module to be valid
-	for PVID in 'prepend-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>'; do # path variable identifier expressions
+	for PVID in 'prepend-path .*PATH\>' 'setenv .*DIR\>' 'setenv .*_SRC\>' 'setenv .*BASE\>'; do # path variable identifier expressions
 	if test -f ${MF} && grep -l '#%Module' >/dev/null ${MF}  && \
 		MPL="`module show ${MF} | sed 's/\s\s*/ /g' | grep "^${PVID} .*$" | grep -v '^\(--\|module-whatis\|  *\)'  `" && \
 		MC="`echo ${MPL} | awk  -F ' ' '{print $1 }'`" && \
 		MI="`echo ${MPL} | awk  -F ' ' '{print $2 }'`" && \
-		MV="`echo ${MPL} | awk  -F ' ' '{print $3 }'`"  && \
-		MA=`echo "${MC} ${MI}" | grep "${PVID}" ` && \
+		MV="`echo ${MPL} | awk  -F ' ' '{print $3 }'`" && \
+		MA=`echo "${MC} ${MI}" | grep "${PVID}" `      && \
 		test -n "$MA" ; # matching assignment
 		then
 		for PD in ${MV//:/ }; do # path directory
