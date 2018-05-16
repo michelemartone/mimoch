64ac6989e4c5e4d8b967e1fa479daf0606a69bb9
Michele Martone <michelemartone@users.sourceforge.net>
2018-05-16 15:36:44 +0200

    licensing info

--- mimoch.sh
+++ mimoch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-#A modulefiles checking script -- INCOMPLETE, FOR PERSONAL USE -- please do not distribute.
-#Author: Michele Martone
+# 
+# Copyright 2018 Michele MARTONE
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#  
+#  
+# Licensing: MIT (see e.g. https://opensource.org/licenses/MIT)
+#  
 #set -x
 set -e
 DEV_NULL=/dev/null
